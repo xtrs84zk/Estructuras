666ccc83757e9b0e3de59f003afe9058e603eae0
xtrs84zk <el.xyzko@gmail.com>
2016-09-30 00:02:22 -0500

    .

--- BalanceoDeParentesis.java
+++ BalanceoDeParentesis.java
@@ -3,35 +3,35 @@ import estructuras.StackUnlimited;
 
 import java.util.Scanner;
 public class BalanceoDeParentesis{
-    public static void main (String[]args){
-        /* Declaración de variables */
-        Stack separadores;
-        char auxiliar, temporal;
-        boolean estaBalanceado = false;
-        String expresion;
-        /* Creación de objetos */
-        Scanner entrada = new Scanner(System.in);
-        separadores = new StackUnlimited();
-        /* Inicializando objetos */
-        System.out.println("Introduzca la expresión a verificar: ");
-        expresion = entrada.next();
-            for(int i = 0; i<expresion.length(); i++){
-                auxiliar = expresion.charAt(i);
-                if(auxiliar == '(' || auxiliar == '[' || auxiliar == '{'){
-                    separadores.push(expresion.charAt(i));
-                }else {
-                	try{
-                    temporal = (Character) separadores.pop();
-                    if((expresion.charAt(i) == ')' && temporal == '(' ) || (temporal == '{' && expresion.charAt(i) == '}') || (temporal == '[' && expresion.charAt(i) == ']')){
-                    	estaBalanceado = true;
-                    } else {
-                        estaBalanceado = false; break; 
+        public static void main (String[]args){
+            /* Declaración de variables */
+            Stack separadores; //Pila que se usará.
+            char auxiliar, temporal; //Variables de apoyo.
+            boolean estaBalanceado = false; //Bandera.
+            String expresion; //Variable donde se almacenará la expresión a evaluar.
+            /* Creación de objetos */
+            Scanner entrada = new Scanner(System.in);
+            separadores = new StackUnlimited();
+            /* Inicializando objetos */
+            System.out.print("Introduzca la expresión a verificar: ");
+            expresion = entrada.next();
+                for(int i = 0; i<expresion.length(); i++){
+                    auxiliar = expresion.charAt(i);
+                    if(auxiliar == '(' || auxiliar == '[' || auxiliar == '{'){
+                        separadores.push(expresion.charAt(i)); //Introduciendo a la pila sólo los valores de apertura
+                    }else {
+                        try{
+                        temporal = (Character) separadores.pop(); //Extrayendo de la pila
+                        if((expresion.charAt(i) == ')' && temporal == '(' ) || (temporal == '{' && expresion.charAt(i) == '}') || (temporal == '[' && expresion.charAt(i) == ']')){
+                            estaBalanceado = true; //La expresión continua estando balanceada.
+                        } else {
+                            estaBalanceado = false; break; //La expresión no está balanceada.
+                        }
+                }catch(Exception e){ //Si el programa termina de forma anormal, significa que algo no coincidió.
+                        System.out.print("La expresión "+ expresion + " no está balanceada.");
                     }
-                }catch(Exception e){
-                	System.out.print("La expresión "+ expresion + " no está balanceada.");
-            	}
+                    }
                 }
-            }
-        if(estaBalanceado){ System.out.print("La expresión " + expresion + " está balanceada.");}
-    }
+            if(estaBalanceado) System.out.print("La expresión " + expresion + " está balanceada.");
+        }
 }
