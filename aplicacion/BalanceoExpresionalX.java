41c5cea96f34ac51f6cbdced365e1d6bc71ae99f
xtrs84zk <el.xyzko@gmail.com>
2016-10-01 15:23:26 -0500

    .

--- aplicacion/BalanceoExpresionalX.java
+++ aplicacion/BalanceoExpresionalX.java
@@ -10,12 +10,12 @@ import java.util.Scanner;
  */
 public class BalanceoExpresionalX {
     public static void main(String[] args) { //Declaración de variables
-        Stack separadores;
-        char auxiliar, temporal;
+
+        char auxiliar;
         String expresion;
         //Creación de objetos
         Scanner entrada = new Scanner(System.in);
-        separadores = new StackUnlimited();
+
         //Inicializando objetos
         System.out.print("\nIntroduzca la expresión a verificar: ");
         expresion = entrada.next();
@@ -26,13 +26,30 @@ public class BalanceoExpresionalX {
 
     }
 
-    public static boolean estaBalanceada(String expresion) {
-        if (expresion.length() == 0) {
-            return true;
-        } else {
-            do {
+    private static boolean estaBalanceada(String expresion) throws Exception {
+        Stack separadores;
+        char temporal, auxiliar;
+        separadores = new StackUnlimited();
+        int i = 0;
+        if (expresion.length() == 0) return true; //Naturalmente, no se puede evaluar una expresión vacía
+        if (expresion.length() % 2 != 0)
+            return false; //Si la expresión tiene una cantidad impar de carácteres, podemos obviar que no está balanceada.
 
-            } while (true);
+        for (int i = 0; i < expresion.length(); i++) {
+            if (auxiliar == '(' || auxiliar == '[' || auxiliar == '{') { //Si el caracter es de apertura, se agrega a la pila.
+                separadores.push(expresion.charAt(i));
+            }
+            try {
+                temporal = (Character) separadores.pop(); //Los caracteres de apertura son sacados de la pila para compararles con los de cierre.
+                if ((expresion.charAt(i) == ')' && temporal != '(') || (temporal == '{' && expresion.charAt(i) != '}') || (temporal == '[' && expresion.charAt(i) != ']')) {
+                    return false;
+                }
+            } catch (Exception e) {
+
+            }
         }
+
+        return true;
     }
+
 }
