447ed855010dc2651a20f22d54dc370e394eb27a
xtrs84zk <el.xyzko@gmail.com>
2016-10-02 16:52:11 -0500

    .

--- aplicacion/BalanceoDeAgrupadores.java
+++ aplicacion/BalanceoDeAgrupadores.java
@@ -16,7 +16,6 @@ public class BalanceoDeAgrupadores {
         Scanner entrada = new Scanner(System.in); //Inicializando objeto Scanner
         System.out.print("\nIntroduzca la expresón a verificar: "); // Pidiendo la expresión
         expresion = entrada.next(); //Asignando el valor de expresión.
-        expresion = expresion.replaceAll(" ", ""); //Quitando espacios en blanco
         expresion = liberaAgrupadores(expresion); //Quitando carácteres que no sean agrupadores
         if (estaBalanceada(expresion)) { //Llamada al método que evalúa la expresión y mostrando resultados
             System.out.println("La expresion está balanceada.");
@@ -32,27 +31,22 @@ public class BalanceoDeAgrupadores {
      **/
     private static boolean estaBalanceada(String expresion) {
         Stack parentesis, llaves, corchetes;
-        int agrupadores[] = new int[3];
         if (expresion.length() == 0) return true; //Naturalmente, no se puede evaluar una expresión vacía
+        if (expresion.length() % 2 != 0) {return false; }//Si la expresión tiene una cantidad impar de carácteres, podemos obviar que no está balanceada.
+        //La pila no está vacía y tiene una cantidad par de carácteres, se procede a evaluar rigurosamente.
         parentesis = new StackUnlimited(); //Suponiendo que no esté vacía, procedemos a crear las pilas
         llaves = new StackUnlimited();
         corchetes = new StackUnlimited();
-        if (expresion.length() % 2 != 0)
-            return false; //Si la expresión tiene una cantidad impar de carácteres, podemos obviar que no está balanceada.
-        //La pila no está vacía y tiene una cantidad par de carácteres, se procede a evaluar rigurosamente.
-        for (int i = 0; i < agrupadores.length; i++) { //Inicializando cantidad de agrupadores en cero.
-            agrupadores[i] = 0;
-        }
         try{
             for (int i = 0; i < expresion.length(); i++) {
                 switch (expresion.charAt(i)) {
                     //Parentesis
                     case '(': parentesis.push(1);
                     case ')': parentesis.pop();
-                        //Llaves
+                    //Llaves
                     case '{': llaves.push(1);
                     case '}': llaves.pop();
-                        //Corchetes
+                    //Corchetes
                     case '[': corchetes.push(1);
                     case ']': corchetes.pop();
                 }
@@ -68,12 +62,12 @@ public class BalanceoDeAgrupadores {
      * Método que libera los agrupadores eliminando el resto de los carácteres.
      **/
     private static String liberaAgrupadores(String expresion) {
+        expresion = expresion.replaceAll(" ", ""); //Quitando espacios en blanco
         String temporal = "";
         for (int i = 0; i < expresion.length(); i++) {
             if (expresion.charAt(i) == '(' || expresion.charAt(i) == '{' || expresion.charAt(i) == '[' || expresion.charAt(i) == ']' || expresion.charAt(i) == '}' || expresion.charAt(i) == ')') {
                 temporal += expresion.charAt(i);
             }
-
         }
         return temporal;
     }
