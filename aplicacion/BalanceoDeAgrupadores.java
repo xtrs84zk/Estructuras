7e1e3a50be8f938d87e474fb14ae352af2a9d2c9
xtrs84zk <el.xyzko@gmail.com>
2016-10-03 00:46:08 -0500

    .

--- aplicacion/BalanceoDeAgrupadores.java
+++ aplicacion/BalanceoDeAgrupadores.java
@@ -11,11 +11,11 @@ import java.util.Scanner;
 //:v
 public class BalanceoDeAgrupadores {
     public static void main(String[] args) {
-        String expresion, auxiliar; //Declaración de variables
-        Scanner entrada = new Scanner(System.in); //Inicializando objeto Scanner
-        System.out.print("\nIntroduzca la expresón a verificar: "); // Pidiendo la expresión
+        String expresion, auxiliar; //Declaración de variables.
+        Scanner entrada = new Scanner(System.in); //Inicializando objeto Scanner.
+        System.out.print("\nIntroduzca la expresón a verificar: "); // Pidiendo la expresión.
         expresion = entrada.next(); //Asignando el valor de expresión.
-        auxiliar = expresion;
+        auxiliar = expresion; //Salvando la expresión sin modificar.
         if (estaBalanceada(expresion)) { //Llamada al método que evalúa la expresión.
             System.out.println("La expresion \"" + auxiliar + "\" está balanceada.");
         } else {
@@ -30,55 +30,52 @@ public class BalanceoDeAgrupadores {
      **/
     private static boolean estaBalanceada(String expresion) {
         Stack parentesis, llaves, corchetes;
-        liberaAgrupadores(expresion); //Quitando carácteres que no sean agrupadores
-       if (expresion.length() == 0) { return false; }//Naturalmente, no se puede evaluar una expresión vacía
+        liberaAgrupadores(expresion); //Quitando carácteres que no sean agrupadores.
+        if (expresion.length() == 0) { return false; }//Naturalmente, no se puede evaluar una expresión vacía.
         if (expresion.length() % 2 != 0) {return false; }//Si la expresión tiene una cantidad impar de carácteres, podemos obviar que no está balanceada.
         //La pila no está vacía y tiene una cantidad par de carácteres, se procede a evaluar rigurosamente.
-        parentesis = new StackUnlimited(); //Procedemos a crear las pilas
+        parentesis = new StackUnlimited(); //Procedemos a crear las pilas, una para cada agrupador.
         llaves = new StackUnlimited();
         corchetes = new StackUnlimited();
-        boolean bandera = false; //La expresión no está balanceada hasta que se demuestre lo contrario
+        boolean bandera = false; //La expresión no está balanceada hasta que se demuestre lo contrario.
         try{ //Se intenta insertar y extraer de la pila.
             for (int i = 0; i < expresion.length(); i++) {
-                if(expresion.charAt(i) == '('){
+                if(expresion.charAt(i) == '('){ //Parentesis
                     parentesis.push(expresion.charAt(i));
-                }else
-                if(expresion.charAt(i) == ')'){
+                }else if(expresion.charAt(i) == ')'){
                     parentesis.pop();
                 }
-                if(expresion.charAt(i) == '{'){
+                if(expresion.charAt(i) == '{'){ //Llaves
                     llaves.push(expresion.charAt(i));
-                }else
-                if(expresion.charAt(i) == '}'){
+                }else if(expresion.charAt(i) == '}'){
                     llaves.pop();
                 }
-                if(expresion.charAt(i) == '['){
+                if(expresion.charAt(i) == '['){//Corchetes
                     corchetes.push(expresion.charAt(i));
-                }else
-                if(expresion.charAt(i) == ']'){
+                }else if(expresion.charAt(i) == ']'){
                     corchetes.pop();
                 }
             }
         } catch(Exception e){ //En caso de haber una excepción, la expresión no está balanceada.
             bandera   = false;
         }
-        //return true; //Si el proceso terminó como debería; la expresión está balanceada.
+        //Ahora que el proceso ha terminado, sólo queda verificar que las pilas estén vacías.
         if(parentesis.isEmpty()  && llaves.isEmpty() && corchetes.isEmpty()){
             bandera = true;
         }
-        return bandera; //regresando el resultado
+        return bandera; //Regresando el resultado.
     }
 
 
     /** Método que libera los agrupadores eliminando el resto de los carácteres. **/
     private static void liberaAgrupadores(String expresion) {
-        expresion = expresion.replaceAll(" ", ""); //Quitando espacios en blanco
+        expresion = expresion.replaceAll(" ", ""); //Quitando espacios en blanco.
         String temporal = "";
-        for (int i = 0; i < expresion.length(); i++) { //Almacenando únicamente los agrupadores
+        for (int i = 0; i < expresion.length(); i++) { //Almacenando únicamente los agrupadores.
             if (expresion.charAt(i) == '(' || expresion.charAt(i) == '{' || expresion.charAt(i) == '[' || expresion.charAt(i) == ']' || expresion.charAt(i) == '}' || expresion.charAt(i) == ')') {
                 temporal += expresion.charAt(i);
             }
-            expresion = temporal;
+            expresion = temporal; //Asignándole el nuevo valor a expresion.
         }
     }
 }
