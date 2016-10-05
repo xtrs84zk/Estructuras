061b8b9d4eb95a950ba71f0189cb2ad49e0471fe
Xavier Sánchez <el.xyzko@gmail.com>
2016-10-05 17:25:34 -0500

    Eliminado el método "liberaAgrupadores()" ya que su existencia siempre fue innecesaria(además de estar mal implementado) No se comprobará que la longitud de la expresión sea mayor a cero, ya que, ahora que el método "liberaAgrupadores()" no existe más, es imposible que la expresión llegue vacía. Se corrigió la dicción y ortografía en algunos comentarios.

--- aplicacion/BalanceoDeAgrupadores.java
+++ aplicacion/BalanceoDeAgrupadores.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class BalanceoDeAgrupadores {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in); //Inicializando objeto Scanner.
-        System.out.print("\nIntroduzca la expresón a verificar: "); // Pidiendo la expresión.
+        System.out.print("\nIntroduzca la expresión a verificar: "); // Pidiendo la expresión.
         String expresion = entrada.nextLine(); //Almacenando la expresión introducida por el usuario.
         if (estaBalanceada(expresion)) { //Llamada al método que evalúa la expresión.
             System.out.println("La expresión está balanceada.");
@@ -29,33 +29,33 @@ public class BalanceoDeAgrupadores {
             return false; //Si la expresión tiene una cantidad impar de carácteres, podemos obviar que no está balanceada.
         }
         if (expresion.charAt(0) == '}' || expresion.charAt(0) == ']' || expresion.charAt(0) == ')') {
-            return false; //Si la expresión comienza en un caracter de cierre, no puede estar balanceada
+            return false; //Si la expresión comienza en un caracter de cierre, no puede estar balanceada.
         }
         //La pila no está vacía y tiene una cantidad par de carácteres, se procede a evaluar rigurosamente.
         agrupadores = new StackUnlimited(); //Procedemos a crear las pilas, una para cada agrupador.
         boolean bandera = false; //La expresión no está balanceada hasta que se demuestre lo contrario.
         try { //Se intenta insertar y extraer de la pila.
             for (int i = 0; i < expresion.length(); i++) {
-                if (expresion.charAt(i) == '(') { //Si entra un paréntesis que abre
+                if (expresion.charAt(i) == '(') { //Si entra un paréntesis que abre.
                     agrupadores.push(expresion.charAt(i)); //Se hace push a la pila con ese valor.
-                } else if (expresion.charAt(i) == ')' && (Character) agrupadores.top() == '(') { //Si entra un paréntesis que cierra y lo último que abrió era un paréntesis
+                } else if (expresion.charAt(i) == ')' && (Character) agrupadores.top() == '(') { //Si entra un paréntesis que cierra y lo último en abrir fue un paréntesis.
                     agrupadores.pop(); //Se elimina el valor al tope de la pila.
                 } else if (expresion.charAt(i) == ')' && (Character) agrupadores.top() != '(') {//Si se intenta cerrar un paréntesis y no había uno abierto
                     return false;//La expresión no está balanceada
                 }
                 if (expresion.charAt(i) == '{') { //Si el caracter es una llave que abre
                     agrupadores.push(expresion.charAt(i)); //Se inserta a la pila el valor '{'
-                } else if (expresion.charAt(i) == '}' && (Character) agrupadores.top() == '{') { //Si entra una llave que cierra y la última que abrió fue una llave
+                } else if (expresion.charAt(i) == '}' && (Character) agrupadores.top() == '{') { //Si entra una llave que cierra y lo último en abrir fue una llave.
                     agrupadores.pop(); //Se elimina el valor al tope de la pila.
-                } else if (expresion.charAt(i) == '}' && (Character) agrupadores.top() != '{') { //Si entra una llave que cierra y no hubo una abierta antes
+                } else if (expresion.charAt(i) == '}' && (Character) agrupadores.top() != '{') { //Si entra una llave que cierra y no hubo una abierta antes.
                     return false; //La expresión no está balanceada
                 }
                 if (expresion.charAt(i) == '[') {//Si el caracter es un corchete que abre
                     agrupadores.push(expresion.charAt(i)); //Se hace push a la pila con ese valor.
-                } else if (expresion.charAt(i) == ']' && (Character) agrupadores.top() == '[') { //Si entra un corchete que abre y lo último que abrió fue un corchete
+                } else if (expresion.charAt(i) == ']' && (Character) agrupadores.top() == '[') { //Si entra un corchete que abre y lo último en abrir fue un corchete.
                     agrupadores.pop();//Se elimina el valor al tope de la pila.
-                } else if (expresion.charAt(i) == ']' && (Character) agrupadores.top() != '[') {//Si entra un corchete que cierra y no hubo uno abierto
-                    return false;//La expresión no está balanceada
+                } else if (expresion.charAt(i) == ']' && (Character) agrupadores.top() != '[') {//Si entra un corchete que cierra y no hubo uno abierto.
+                    return false;//La expresión no está balanceada.
                 }
             }
         } catch (Exception e) { //En caso de haber una excepción, la expresión no está balanceada.
