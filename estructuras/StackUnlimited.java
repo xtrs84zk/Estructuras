435de1491ae4fb89e6a30b766adb922c982a42b7
el.xyzko@gmail.com <1497k22c>
2016-10-27 11:21:59 -0500

    .

--- estructuras/StackUnlimited.java
+++ estructuras/StackUnlimited.java
@@ -15,45 +15,33 @@ public class StackUnlimited implements Stack{
         stack = new Object[0];
     }
 
-    /**
-     * Método push que "pushea" (inserta) nuevos elementos a la pila.
-     */
+    /** Método push que "pushea" (inserta) nuevos elementos a la pila. **/
     public void push (Object elemento){
         Object[] auxiliar;
         auxiliar = new Object[stack.length+1];
-        for(int i= 0; i<stack.length; i++){
-            auxiliar[i] = stack[i];
-        }
+        System.arraycopy(stack, 0, auxiliar, 0, stack.length);
         auxiliar[auxiliar.length-1] = elemento;
         stack = auxiliar;
     }
     
-    /**
-     * Método que "popea" (extrae y elimina) el valor al tope de la pila
-     * @return elemento al tope de la pila
-     */
+    /** Método que "popea" (extrae y elimina) el valor al tope de la pila
+     * @return elemento al tope de la pila. **/
     public Object pop () throws Exception{
         Object auxiliarObject, auxiliarArray[];
         auxiliarObject = stack[stack.length-1];
         auxiliarArray = new Object[stack.length-1];
-        for(int i= 0; i<stack.length-1; i++){
-            auxiliarArray[i] = stack[i];
-        }
+        System.arraycopy(stack, 0, auxiliarArray, 0, stack.length - 1);
         stack = auxiliarArray;
         return auxiliarObject;
     }
     
-    /**
-     * Método que regresa el elemento al tope de la pila sin eliminarlo
-     * @return elemento al tope
-     */
+    /** Método que regresa el elemento al tope de la pila sin eliminarlo
+     * @return elemento al tope. **/
     public Object top() throws Exception{
         return stack[stack.length-1];
     }
     
-    /**
-     * Método que dice si la pila está vacía o no.
-     */
+    /** Método que dice si la pila está vacía o no. **/
     public boolean isEmpty(){
         return stack.length==0;
     }
