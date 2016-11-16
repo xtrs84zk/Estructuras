5922122cef82f89eaa09c7e3600f48e1f136f14f
xtrs84zk <el.xyzko@gmail.com>
2016-11-15 21:53:00 -0600

    Nodos

--- estructuras/ListaSimple.java
+++ estructuras/ListaSimple.java
@@ -4,24 +4,38 @@ package estructuras;
 public class ListaSimple {
     private NodoListaSimple primero;
 
+    /** Constructor para la clase ListaSimple que inicializa los valores en null. **/
     public ListaSimple() {
         primero = null;
     }
-
+    /** Constructor para la clase ListaSimple que inicializa los valores con
+     * parámetros definidos por el usuario.
+     * @param dato que es lo que contendrá el primer nodo en la lista.**/
     public ListaSimple(Object dato) {
+        //Se crea el primer nodo con el dato recibido en su interior.
         this.primero = new NodoListaSimple(dato);
     }
-
+    /** Método que inserta un valor al inicio de una lista enlazada.
+     * @param dato que es el dato a insertar en el nuevo nodo de la lista. **/
     public void insertarAlInicio(Object dato){
+        //Se crea un nuevo NodoListaSimple para almacenar el dato recibido.
         NodoListaSimple nuevoNodo = new NodoListaSimple(dato);
+        //Se establece el nodo actualmente en primer lugar como el siguiente.
         nuevoNodo.setSiguiente(primero);
+        //Se cambia la referencia del primer nodo hacía el nuevo nodo.
         primero = nuevoNodo;
     }
-
+    /** Método que elimina el valor (y el nodo) que se encuentre al inicio de
+     *  una lista enlazada. Al final regresa dicho valor.
+     *  @return temporal.getDato() que es lo que contenía el nodo eliminado.**/
     public Object eliminarAlInicio(){
+        //Se crea un nuevo NodoListaSimple para almacenar el nodo a eliminar.
         NodoListaSimple temporal;
+        //Se copia la referencia de primero hacía temporal.
         temporal = primero;
+        //Se establece el primer nodo de la lista en el segundo.
         primero = primero.getSiguiente();
+        //Se regresa el contenido del nodo eliminado.
         return temporal.getDato();
     }
 }
--- estructuras/NodoListaSimple.java
+++ estructuras/NodoListaSimple.java
@@ -1,35 +1,50 @@
 package estructuras;
 
-/** Created by Xavier Sánchez on 15/11/2016. **/
+/** Created by Javier Sánchez on 15/11/2016. **/
 public class NodoListaSimple {
     private Object dato;
     private NodoListaSimple siguiente;
-
+    /** Constructor para la clase NodoListaSimple que inicializa los
+     * valores con los parámetros recibidos. Sólo inserta el dato.
+     * @param dato que es el dato a insertar en el nodo.**/
     public NodoListaSimple(Object dato) {
         this.dato = dato;
     }
-
+    /** Constructor para la clase NodoListaSimple que inicializa los
+     * valores con los parámetros recibidos. Inserta el contenido y
+     * especifica cuál será el siguiente nodo.
+     * @param dato que es el dato a insertar en el nodo.
+     * @param siguiente que es el siguiente nodo.**/
     public NodoListaSimple(Object dato, NodoListaSimple siguiente) {
         this.dato = dato;
         this.siguiente = siguiente;
     }
 
+    /** Método getDato que regresa el contenido del nodo.
+     * @return dato que es el contenido del nodo. **/
     public Object getDato() {
         return dato;
     }
 
+    /** Método setDato que establece el contenido del nodo.
+     * @param dato que es el contenido que tendrá el nodo. **/
     public void setDato(Object dato) {
         this.dato = dato;
     }
 
+    /** Método getSiguiente que regresa el siguiente nodo.
+     * @return siguiente que es el siguiente nodo. **/
     public NodoListaSimple getSiguiente() {
         return siguiente;
     }
 
+    /** Método setSiguiente que establece el nodo que seguirá.
+     * @param siguiente que es el siguiente nodo en la lista. **/
     public void setSiguiente(NodoListaSimple siguiente) {
         this.siguiente = siguiente;
     }
 
+    /** Método toString que regresa un resumen del contenido del nodo.**/
     public String toString() {
         return "NodoListaSimple{" +
                 "dato=" + dato +
