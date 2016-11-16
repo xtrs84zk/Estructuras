15652ab3bc7335b21975dc04ac004233e2183d1a
xtrs84zk <el.xyzko@gmail.com>
2016-11-16 14:04:46 -0600

    Nodos

--- estructuras/ListaSimple.java
+++ estructuras/ListaSimple.java
@@ -1,23 +1,35 @@
 package estructuras;
 
-/** Created by Javier Sánchez on 15/11/2016. **/
+/**
+ * Created by Javier Sánchez on 15/11/2016.
+ **/
 public class ListaSimple {
     private NodoListaSimple primero;
 
-    /** Constructor para la clase ListaSimple que inicializa los valores en null. **/
+    /**
+     * Constructor para la clase ListaSimple que inicializa los valores en null.
+     **/
     public ListaSimple() {
         primero = null;
     }
-    /** Constructor para la clase ListaSimple que inicializa los valores con
+
+    /**
+     * Constructor para la clase ListaSimple que inicializa los valores con
      * parámetros definidos por el usuario.
-     * @param dato que es lo que contendrá el primer nodo en la lista.**/
+     *
+     * @param dato que es lo que contendrá el primer nodo en la lista.
+     **/
     public ListaSimple(Object dato) {
         //Se crea el primer nodo con el dato recibido en su interior.
         this.primero = new NodoListaSimple(dato);
     }
-    /** Método que inserta un valor al inicio de una lista enlazada.
-     * @param dato que es el dato a insertar en el nuevo nodo de la lista. **/
-    public void insertarAlInicio(Object dato){
+
+    /**
+     * Método que inserta un valor al inicio de una lista enlazada.
+     *
+     * @param dato que es el dato a insertar en el nuevo nodo de la lista.
+     **/
+    public void insertarAlInicio(Object dato) {
         //Se crea un nuevo NodoListaSimple para almacenar el dato recibido.
         NodoListaSimple nuevoNodo = new NodoListaSimple(dato);
         //Se establece el nodo actualmente en primer lugar como el siguiente.
@@ -25,10 +37,14 @@ public class ListaSimple {
         //Se cambia la referencia del primer nodo hacía el nuevo nodo.
         primero = nuevoNodo;
     }
-    /** Método que elimina el valor (y el nodo) que se encuentre al inicio de
-     *  una lista enlazada. Al final regresa dicho valor.
-     *  @return temporal.getDato() que es lo que contenía el nodo eliminado.**/
-    public Object eliminarAlInicio(){
+
+    /**
+     * Método que elimina el valor (y el nodo) que se encuentre al inicio de
+     * una lista enlazada. Al final regresa dicho valor.
+     *
+     * @return temporal.getDato() que es lo que contenía el nodo eliminado.
+     **/
+    public Object eliminarAlInicio() {
         //Se crea un nuevo NodoListaSimple para almacenar el nodo a eliminar.
         NodoListaSimple temporal;
         //Se copia la referencia de primero hacía temporal.
@@ -37,5 +53,20 @@ public class ListaSimple {
         primero = primero.getSiguiente();
         //Se regresa el contenido del nodo eliminado.
         return temporal.getDato();
+    }
+
+    public void insertarAlFinal(Object dato){
+        //Declarar una nueva referencia para, a partir del primer nodo, moverla
+        // hasta el último nodo sin modificar el primero.
+        NodoListaSimple nuevoNodo = new NodoListaSimple(dato);
+        NodoListaSimple ultimo = primero;
+        while(ultimo.getSiguiente() != null){
+            ultimo = ultimo.getSiguiente();
+        }
+        ultimo.setSiguiente(nuevoNodo);
+    }
+
+    public boolean isEmpty(){
+        return primero == null;
     }
 }
---
+++ estructuras/PilaConLista.java
@@ -0,0 +1,24 @@
+package estructuras;
+
+/** Created by Javier Sánchez on 16/11/2016. **/
+public class PilaConLista implements Stack {
+
+    public void push(Object elemento) {
+
+    }
+
+
+    public Object pop() throws Exception {
+        return null;
+    }
+
+
+    public Object top() throws Exception {
+        return null;
+    }
+
+
+    public boolean isEmpty() {
+        return false;
+    }
+}
