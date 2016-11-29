8c42b94b986d77243d910d3e8e4ee1a56786a90a
xtrs84zk <el.xyzko@gmail.com>
2016-11-28 20:34:26 -0600

    Nodos

--- estructuras/ListaSimple.java
+++ estructuras/ListaSimple.java
@@ -55,33 +55,42 @@ public class ListaSimple {
         return temporal.getDato();
     }
 
-    /** Método insertarAlFinal que inserta un elemento al final de una lista ordenada.
+    /**
+     * Método insertarAlFinal que inserta un elemento al final de una lista ordenada.
      *
-     * @param dato que es el dato a insertar en la lista.**/
-    public void insertarAlFinal(Object dato){
-        //Declarar una nueva referencia para, a partir del primer nodo, moverla
-        // hasta el último nodo sin modificar el primero.
+     * @param dato que es el dato a insertar en la lista.
+     **/
+    public void insertarAlFinal(Object dato) {
+        //Declarar una nueva referencia para, a partir del primer nodo, se mueva
+        // hacia el último nodo sin modificar el primero.
         NodoListaSimple nuevoNodo = new NodoListaSimple(dato);
-        NodoListaSimple ultimo = primero;
-        //Se busca el último nodo en la lista.
-        while(ultimo.getSiguiente() != null){
-            ultimo = ultimo.getSiguiente();
+        if (!isEmpty()) {
+            NodoListaSimple ultimo = primero;
+            //Se busca el último nodo en la lista.
+            while (ultimo.getSiguiente() != null) {
+                ultimo = ultimo.getSiguiente();
+            }
+            //Una vez encontrado, se establece una referencia hacia el nuevo nodo.
+            ultimo.setSiguiente(nuevoNodo);
+        } else {
+            primero = nuevoNodo;
         }
-        //Una vez encontrado, se establece una referencia hacia el nuevo nodo.
-        ultimo.setSiguiente(nuevoNodo);
     }
 
-    /** Método eliminarElUltimo encargado de eliminar el último elemento en
+    /**
+     * Método eliminarElUltimo encargado de eliminar el último elemento en
      * una lista ordenada. Lanza una excepción en caso de no encontrar elementos.
-     * @return contenidoDelUltimoNodo que es el contenido del nodo eliminado.**/
+     *
+     * @return contenidoDelUltimoNodo que es el contenido del nodo eliminado.
+     **/
     public Object eliminarElUltimo() throws Exception {
         //Variable donde se almacenará el contenido del elemento eliminado
         Object contenidoDelUltimoNodo;
         //Para eliminar el elemento al final de la lista, primeramente se verifica
         // que haya elementos a eliminar.
-        if(!isEmpty()) {
+        if (!isEmpty()) {
             //En caso de haber sólo un elemento en la lista, se elimina directamente.
-            if(primero.getSiguiente() == null){
+            if (primero.getSiguiente() == null) {
                 //En caso de que sólo haya un elemento en la lista, se almacena su contenido.
                 contenidoDelUltimoNodo = primero.getDato();
                 primero = null;
@@ -106,9 +115,12 @@ public class ListaSimple {
         return contenidoDelUltimoNodo;
     }
 
-    /** Método isEmpty que verifica que la lista esté vacía.
-     * @return true en caso de que la lista no contenga elementos. **/
-    public boolean isEmpty(){
+    /**
+     * Método isEmpty que verifica que la lista esté vacía.
+     *
+     * @return true en caso de que la lista no contenga elementos.
+     **/
+    public boolean isEmpty() {
         return primero == null;
     }
 
@@ -126,7 +138,7 @@ public class ListaSimple {
         while (temporal != null) {
             // Se verifica que el contenido de la referencia actual sea igual al
             // elemento a comparar, en caso de serlo, se regresa true.
-            if (temporal.equals(elementoABuscar)) {
+            if (temporal.getDato().equals(elementoABuscar)) {
                 return true;
             }
             // Si los contenidos no son iguales, se mueve la referencia hacia el
