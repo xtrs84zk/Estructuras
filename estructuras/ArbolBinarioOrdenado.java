fe43adaee7c31e6eccec7261f0c8e80293e5675a
xtrs84zk <el.xyzko@gmail.com>
2016-12-01 17:04:18 -0600

    Cola con lista / documentación

--- estructuras/ArbolBinarioOrdenado.java
+++ estructuras/ArbolBinarioOrdenado.java
@@ -4,16 +4,28 @@ package estructuras;
  * Created by xtrs84zk on 23/11/2016.
  **/
 public class ArbolBinarioOrdenado {
+    //Nodo de tipo árbol binario donde se encontrará la
+    // referencia al nodo raíz del árbol
     private NodoArbolBinario raiz;
 
+    /**
+     * Constructor para la clase ArbolBinarioOrdenado
+     * que inicializa los valores en null.
+     **/
     public ArbolBinarioOrdenado() {
         raiz = null;
     }
 
+    /** Constructor para la clase ArbolBinarioOrdenado
+     * que inicializa los valore con parametros dados.
+     * @param dato que es el dato a insertar en la raíz.**/
     public ArbolBinarioOrdenado(Object dato) {
         raiz = new NodoArbolBinario(dato, null, null);
     }
 
+    /** Método insertar que verifica qué procedimiento
+     * utilizar para insertar los datos en el árbol.
+     * @param dato que es el dato a insertar.**/
     public void insertar(Object dato) {
 
     }
@@ -42,24 +54,45 @@ public class ArbolBinarioOrdenado {
         }
     }
 
+    /** Método que recorre el árbol en modo inOrden
+     * Primeramente resuelve el nodo a la izquierda,
+     * luego la raíz y finalmente, el nodo a la derecha.
+     * @param raiz que la raíz del subárbol a procesar.**/
     private void inOrdenRecursivo(NodoArbolBinario raiz) {
-        inOrdenRecursivo(raiz.getIzquierdo());
+        //En caso de que el subárbol actual tenga una referencia
+        //válida hacia el nodo izquierdo, se recorre dicho nodo.
+        if (raiz.getIzquierdo() != null) {
+            inOrdenRecursivo(raiz.getIzquierdo());
+        }
         //procesar la raíz.
-        inOrdenRecursivo(raiz.getDerecho());
+        //Se recorre el subárbol a la derecha si y sólo si existe.
+        if (raiz.getDerecho() != null) {
+            inOrdenRecursivo(raiz.getDerecho());
+        }
     }
 
+    /** Método público que se encarga de verificar que el
+     * método recursivo postOrdenRecursivo pueda procesar
+     * el árbol que se le ha enviado. **/
     public void postOrden() {
         if (!isEmpty()) {
             postOrdenRecursivo(raiz);
         }
     }
 
+    /** Método que recorre el árbol binario procesando primero
+     * los nodos a la izquierda, después los de la derecha y
+     * finalmente la raíz. Es una función recursiva así que,
+     * en realidad, sólo procesa un subárbol a la vez.
+     * @param raiz que será la raíz del árbol a procesar.**/
     private void postOrdenRecursivo(NodoArbolBinario raiz) {
         postOrdenRecursivo(raiz.getIzquierdo());
         postOrdenRecursivo(raiz.getDerecho());
         //Procesar la raíz.
     }
 
+    /** Método isEmpty que informa si el árbol está vacío.
+     * @return true en caso de que la raíz no haya sido establecida. **/
     public boolean isEmpty() {
         return raiz == null;
     }
---
+++ estructuras/ColaConLista.java
@@ -0,0 +1,76 @@
+package estructuras;
+
+/**
+ * Created by xtrs84zk on 01/12/2016.
+ **/
+public class ColaConLista implements Queue {
+    //Lista simple que actuará como cola.
+    private ListaSimple cola;
+
+    /**
+     * Constructor para la clase ColaConLista
+     * que inicializa la cola vacía.
+     **/
+    public ColaConLista() {
+        cola = new ListaSimple();
+    }
+
+    /**
+     * Método que regresa un booleano dependiendo
+     * de si la cola está vacía o no.
+     *
+     * @return true si la cola está vacía.
+     **/
+    public boolean isEmpty() {
+        return cola.isEmpty();
+    }
+
+    /**
+     * Método que regresa el elemento encontrado
+     * al frente de la cola.
+     *
+     * @return elementoAlFrente
+     **/
+    public Object front() throws Exception {
+        //En caso de que la cola esté vacía,
+        //se procede a lanzar una excepción.
+        if (cola.isEmpty()) {
+            throw new Exception("La cola está vacía.");
+        } else {
+            //Se crea una variable de tipo Object y se le asigna
+            //el valor del último objeto en la lista.
+            Object elementoAlFrente = cola.eliminarElUltimo();
+            //Debido a que el elemento fue eliminado, se vuelve a
+            // insertar en la posición que tenía.
+            cola.insertarAlFinal(elementoAlFrente);
+            //Finalmente, se regresa el elemento.
+            return elementoAlFrente;
+        }
+    }
+
+    /**
+     * Método para insertar un elemento a la cola.
+     *
+     * @param dato que es dato a insertar.
+     **/
+    public void insert(Object dato) {
+        cola.insertarAlInicio(dato);
+    }
+
+    /**
+     * Método que extrae un elemento de la cola.
+     * Lanza una excepción si la cola está vacía.
+     *
+     * @return el elemento que estuviera al frente
+     * de la cola.
+     **/
+    public Object extract() throws Exception {
+        //En caso de que la cola esté vacía
+        if (isEmpty()) {
+            //Se lanza una excepción
+            throw new Exception("La cola está vacía.");
+        }
+        //Se regresa (y elimina) el elemento al frente de la cola.
+        return cola.eliminarElUltimo();
+    }
+}
